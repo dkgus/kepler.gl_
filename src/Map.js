2450848a941384a40cfa22a1f0a97f132ac9b05a
dkgus <dkgus12dkgus@naver.com>
2024-05-13 13:44:33 +0900

    feat: 데이터 표출

--- src/Map.js
+++ src/Map.js
@@ -9,23 +9,41 @@ export default function Map() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("your data set");
+        const response = await fetch(
+          "https://gist.githubusercontent.com/leighhalliday/a994915d8050e90d413515e97babd3b3/raw/a3eaaadcc784168e3845a98931780bd60afb362f/covid19.json"
+        );
         const data = await response.json();
+        console.log("data", data);
 
         dispatch(
           addDataToMap({
             datasets: {
               info: {
-                label: "caregivers",
-                id: "caregivers",
+                label: "Fetched Data",
+                id: "fetched_data",
               },
-              data,
+              data: data,
             },
-            options: {
-              centerMap: true,
-              readOnly: true,
+            config: {
+              version: "v1",
+              config: {
+                visState: {
+                  layers: [
+                    {
+                      type: "3D",
+                      config: {
+                        dataId: "fetched_data",
+                        columns: {
+                          lat: "latitude_field",
+                          lng: "longitude_field",
+                        },
+                        isVisible: true,
+                      },
+                    },
+                  ],
+                },
+              },
             },
-            config: {},
           })
         );
       } catch (error) {
@@ -39,7 +57,6 @@ export default function Map() {
   return (
     <KeplerGl
       mapboxApiAccessToken="pk.eyJ1IjoibW9zaW84OSIsImEiOiJjbHRyYnI4b2cwOHRhMmtwN250cGhyam9xIn0.cWA7FjLYS63ZnDm5VRawug"
-      id="caregiver"
       width={window.innerWidth}
       height={window.innerHeight}
     />
